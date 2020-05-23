e9de13a8555306bb16656753cb74c2e509ef024a
tsuny221 <kingoftsuny@gmail.com>
2020-05-23 07:20:12 +0000

    [Add] password reset

--- sample_app/config/environments/production.rb
+++ sample_app/config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = '<your heroku app>.herokuapp.com'
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
